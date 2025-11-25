1d19af337e56b6e62623244990d7dc02f0700d83
Alexey Moor <asmodeus.1819@gmail.com>
2025-11-25 10:25:50 +0100

    feat: add Bank component and integrate it into Balance display

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -1,12 +1,16 @@
 import {useSelector} from "react-redux";
+import Bank from "./Bank.jsx";
 
 const Balance = () => { // КОМПОНЕНТ для отображения баланса пользователя
   const balance = useSelector(state => state.balance); // useSelector извлекает баланс из состояния Redux
   const quote = useSelector(state => state.quote); // useSelector извлекает цитату из состояния Redux
+  const info = 'Braavos'
+
+  console.log('Render Balance');
 
   return (
     <div className={'text-center text-uppercase'}>
-      <h1 className={'font-bold text-8xl text-[gold]'}>Iron Bank of Braavos</h1>
+      <Bank info={info}/>
       <h3 className={'text-4xl text-[whitesmoke]'}>{quote}</h3>
       <h2 className={'text-6xl text-[firebrick]'}>Your balance: {balance}€</h2>
     </div>
---
+++ src/components/Bank.jsx
@@ -0,0 +1,12 @@
+import {memo} from "react";
+
+const Bank = ({info}) => {
+
+  console.log(`Render Bank ${info}`);
+
+  return (
+    <h1 className={'font-bold text-8xl text-[gold]'}>Iron Bank of Braavos of {info}</h1>
+  );
+};
+
+export default memo(Bank);
