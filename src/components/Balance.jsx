32c668be9063ad420d4f352c2e94c208a4bfc689
Alexey Moor <asmodeus.1819@gmail.com>
2025-11-25 11:14:58 +0100

    feat: refactor Balance component to use useMemo for info management and add random name selection

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -1,18 +1,20 @@
 import {useSelector} from "react-redux";
 import Bank from "./Bank.jsx";
-import {useRef} from "react";
+import {useMemo} from "react";
+import {getRandomIndex, names} from "../utils/constants.js";
 
 const Balance = () => { // КОМПОНЕНТ для отображения баланса пользователя
   const balance = useSelector(state => state.balance); // useSelector извлекает баланс из состояния Redux
   const quote = useSelector(state => state.quote); // useSelector извлекает цитату из состояния Redux
-  // const info = {name: 'Braavos'}
-  const info = useRef({name: 'Braavos'});
+  const index = getRandomIndex(names.length)
+  const info = useMemo(() => ({name: names[index]}), [index]);
 
-  console.log('Render Balance');
+
+  console.log(`Render Balance index: ${index}`);
 
   return (
     <div className={'text-center text-uppercase'}>
-      <Bank info={info}/>
+      <Bank info={info} />
       <h3 className={'text-4xl text-[whitesmoke]'}>{quote}</h3>
       <h2 className={'text-6xl text-[firebrick]'}>Your balance: {balance}€</h2>
     </div>
---
+++ src/utils/constants.js
@@ -0,0 +1,2 @@
+export const names = ['Braavos', 'Reverlands', 'Westerlands', 'Crownlands'];
+export const getRandomIndex = bound => Math.trunc(Math.random() * bound);
